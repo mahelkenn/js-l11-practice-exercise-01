bc6b46ed549c3c879fc180b6b7c04098ab45b527
Michele <mahelkenn@gmail.com>
2021-04-29 18:38:35 -0800

    Added dropdown to select number of users displayed

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("#users");
 
-const getData = async function() {
-    const usersRequest = await fetch("https://randomuser.me/api?results=5");
+const getData = async function(numUsers) {
+    const usersRequest = await fetch(`https://randomuser.me/api?results=${numUsers}`);
     const data = await usersRequest.json();
     const userResults = data.results;
     displayUsers(userResults);
@@ -24,4 +25,9 @@ const displayUsers = function(userResults) {
     };
 
 
-getData();
+getData(1);
+ 
+selectUserNumber.addEventListener("change", function (e) {
+    let numUsers = e.target.value;
+    getData(numUsers);
+});
